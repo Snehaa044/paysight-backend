74417006e04fbf6fba890390b6fce26f374278f9
Snehaa044 <sssneha828@gmail.com>
2026-05-11 00:29:32 +0530

    added src files

---
+++ PaysightApplication.java
@@ -0,0 +1,13 @@
+package com.sneha.paysight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaysightApplication.class, args);
+	}
+
+}
---
+++ PaysightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.paysight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
